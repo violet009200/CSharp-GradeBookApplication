07559b067188252e68c9f2f6b630532cffd017d1
violet009200 <violet009200@gmail.com>
2020-06-08 20:58:14 -0500

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,6 +28,24 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
 
